92461c13d1c2fa3b0813c9c5ca60d079ededf5d3
Jodat <ext.ali.jodat@nak-mci.com>
2023-12-18 17:31:25 +0330

    first commit

---
+++ src/main/java/dev/jodat/osqueryspringboot/OsquerySpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.jodat.osqueryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsquerySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsquerySpringbootApplication.class, args);
+	}
+
+}
